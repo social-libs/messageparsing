be95e00567bcd8d92b257f01d897f3cc7a6b7ac6
Andrija Petrovic <andrija.hers@gmail.com>
2020-05-09 05:33:24 -0400

    Description and og:image introduced in regex parsing

--- htmlparser_regex.js
+++ htmlparser_regex.js
@@ -3,7 +3,10 @@ function createHtmlParser_regex (lib) {
 
   var regexes = {
     title: /<title>.*<\/title>/i,
-    metaogtitle: /<meta[^>]*og:title[^>]*>/i,
+    metaogtitle: /<meta[^>]*og:title\s*=\s*[^>]*>/i,
+    metadescription: /<meta[^>]*['"]{1}description['"]{1}[^>]*>/i,
+    metaogdescription: /<meta[^>]*og:description[^>]*>/i,
+    metaogimage: /<meta[^>]*og:image[^>]*>/i,
     content: /content\s*=\s*["']{1}(.*)["']{1}/i,
     text: />(.*)</
   };
@@ -53,10 +56,10 @@ function createHtmlParser_regex (lib) {
     for (i=0; i<regexnames.length; i++) {
       ret = contentsoftag(string, regexnames[i]);
       if (ret) {
-        console.log('na', regexnames[i], 'nasao', ret);
+        //console.log('na', regexnames[i], 'nasao', ret);
         return ret;
       }
-      console.log('na', regexnames[i], 'nema NISTA');
+      //console.log('na', regexnames[i], 'nema NISTA');
     }
     return '';
   }
@@ -72,6 +75,8 @@ function createHtmlParser_regex (lib) {
     var ret = {};
 
     addiffound(ret, 'title', html, ['metaogtitle', 'title']);
+    addiffound(ret, 'description', html, ['metaogdescription', 'metadescription']);
+    addiffound(ret, 'image', html, ['metaogimage']);
     //console.log(contentsoftags(html, ['metaogtitle', 'title']));
 
     return ret;
--- test/test.htmlregex.js
+++ test/test.htmlregex.js
@@ -1,19 +1,46 @@
-function testIt (htmlsamplefilename) {
-  it('Testing regexparsing of sample '+htmlsamplefilename, function () {
+function testIt (testobj) {
+  it('Testing regexparsing of sample '+testobj.sample, function () {
     var path, htmlsample, ret;
     try {
-      path = require('path').join(__dirname, 'htmlsamples', htmlsamplefilename);
+      path = require('path').join(__dirname, 'htmlsamples', testobj.sample);
       htmlsample = require('fs').readFileSync(path).toString();
     } catch (e) {
-      console.error('Could not read', htmlsamplefilename, ', that was interpreted as', path);
+      console.error('Could not read', testobj.sample, ', that was interpreted as', path);
     }
     ret = parseHtml(htmlsample);
-    console.log(htmlsamplefilename, '=>', ret);
-    htmlsamplefilename = null;
+    console.log(testobj.sample, '=>', ret);
+    ret = expect(ret).to.include(testobj.expect);
+    testobj = null;
+    return ret;
   });
 }
 
-var samples = ['title.html', 'youtube_kids.html', 'w3schools_img.html', 'stackoverflow_1.html']
+var samples = [{
+  sample: 'title.html',
+  expect: {
+    title: 'BLA'
+  }
+},{
+  sample: 'youtube_kids.html',
+  expect: {
+    title: 'Red Yellow Green Blue | + More Kids Songs | Super Simple Songs - YouTube',
+    description: 'Watch videos from Super Simple in the Super Simple App for iOS! ► http://apple.co/2nW5hPd Sing along with The Bumble Nums to this color-themed song for kids,...',
+    image: 'https://i.ytimg.com/vi/v-BvRlsbUiU/maxresdefault.jpg'
+  }
+},{
+  sample: 'w3schools_img.html',
+  expect: {
+    title: 'HTML img tag',
+    description: 'Well organized and easy to understand Web building tutorials with lots of examples of how to use HTML, CSS, JavaScript, SQL, PHP, Python, Bootstrap, Java and XML.'
+  }
+},{
+  sample: 'stackoverflow_1.html',
+  expect: {
+    title: 'javascript - What is a good regular expression to match a URL? - Stack Overflow',
+    description: 'Currently I have an input box which will detect the URL and parse the data.&#xA;&#xA;So right now, I am using:&#xA;&#xA;var urlR = /^(?:([A-Za-z]&#x2B;):)?(\\/{0,3})([0-9.\\-A-Za-z]&#x2B;)&#xA;           (?::(\\d&#x2B;))?(?:\\/([^?#]*))...',
+    image: 'https://cdn.sstatic.net/Sites/stackoverflow/Img/apple-touch-icon@2.png?v=73d79a89bded'
+  }
+}]
 
 describe('Test HTML parsing via regex', function () {
   it('Load html regex parser', function () {
